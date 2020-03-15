c80deb4c9ba8ebfc457d53f74158a87fd53ebee5
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-03-15 21:22:55 +0000

    Automatically backed up by Learn

--- looping.rb
+++ looping.rb
@@ -1,11 +1,1 @@
-require 'pry'
 
-string =  "Hello there, how are you?"
-    
-def reverse_each_word(string)
-      new_array = []
-      binding.pry
-      new_array = string.split("")
-      
-     end
-     
